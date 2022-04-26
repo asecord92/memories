3de06d80cfa6f6fe4426459102c8e9e46ef0e373
Adam <ats92@cox.net>
2022-04-26 15:59:00 -0700

    finished posts

--- client/src/components/posts/Post/Post.js
+++ client/src/components/posts/Post/Post.js
@@ -13,7 +13,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
@@ -49,14 +49,19 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           <ThumbUpALtIcon fontSize="small">{post.likeCount}</ThumbUpALtIcon>
-          Liked
+          &nbsp;Like&nbsp;
+          {post.likeCount}
         </Button>
         <Button
           size="small"
